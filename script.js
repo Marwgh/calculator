3496ef73c78718df3dac63247ff434eb07a8b3f6
marwane <marw0619@stud.kea.dk>
2021-02-07 17:41:41 +0100

    rounding

--- script.js
+++ script.js
@@ -23,26 +23,41 @@ function read () {
     if ( opera === "add") {
         let li = document.createElement("li");
         li.innerHTML = num1 + num2 ;
-        document.querySelector("#results").appendChild(li);
-        document.querySelector("#results").scrollBy(0 , 4454545454545); 
+        roundS (li);
+        
         
     } else if ( opera === "sub" ) {
         let li = document.createElement("li");
         li.innerHTML = num1 - num2 ;
-        document.querySelector("#results").appendChild(li); 
-        document.querySelector("#results").scrollBy(0 , 4454545454545); 
+        roundS (li);
 
     } else if ( opera === "mul" ) {
         let li = document.createElement("li");
         li.innerHTML = num1 * num2 ;
-        document.querySelector("#results").appendChild(li);
-        document.querySelector("#results").scrollBy(0 , 4454545454545);  
+        roundS (li);
 
     } else if ( opera === "div" ) {
         let li = document.createElement("li");
         li.innerHTML = num1 / num2 ;
-        document.querySelector("#results").appendChild(li); 
-        document.querySelector("#results").scrollBy(0 , 4454545454545); 
+        roundS (li); 
 
+    }
+}
+
+function roundS (li) {
+    let check = document.querySelector("#doround") ;
+    let rounder =  document.querySelector("#decimals").value 
+    rounder = Math.pow(10 , rounder );
+    console.log(rounder);
+    if (check.checked == true) {
+        li.innerHTML = Math.round( li.innerHTML * rounder )/ rounder  ;
+        console.log(li);
+        console.log("hio this checked");
+        document.querySelector("#results").appendChild(li);
+        document.querySelector("#results").scrollBy(0 , 4454545454545); 
+    } else  {
+        console.log("hio this not checked");
+        document.querySelector("#results").appendChild(li);
+        document.querySelector("#results").scrollBy(0 , 4454545454545); 
     }
 }
